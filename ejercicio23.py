502d8f509138bd1efb83faa1b1512f5b72cd11ec
Andres Sequeda Higuita <80718240+andressequeda@users.noreply.github.com>
2021-05-09 22:45:17 -0500

    Add files via upload

---
+++ ejercicio23.py
@@ -0,0 +1,11 @@
+numero  =  int ( input ( "Ingrese un número entero:" ))
+si numero  %  2  ==  0 :
+    si  numero  >  0 :
+        print ( "El número es par positivo." )
+    otra cosa :
+        print ( "El número es par negativo." )
+otra cosa :
+    si  numero  >  0 :
+        print ( "El número es impar positivo." )
+    otra cosa :
+        print ( "El número es impar negativo." )
